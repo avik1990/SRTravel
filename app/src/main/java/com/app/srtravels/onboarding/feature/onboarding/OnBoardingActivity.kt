e7a02cb8a384733ac362c1df2c7ce03dbf96ff2f
ADMIN <avik1990@gmail.com>
2023-04-05 02:51:03 +0530

    Initial commit

---
+++ app/src/main/java/com/app/srtravels/onboarding/feature/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,17 @@
+package com.app.srtravels.onboarding.feature.onboarding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.app.srtravels.databinding.OnboardingActivityBinding
+
+
+class OnBoardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+      val  binding = OnboardingActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+}
---
+++ app/src/main/java/com/app/srtravels/splash/Splash.kt
@@ -0,0 +1,24 @@
+package com.app.srtravels.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.app.srtravels.MainActivity
+import com.app.srtravels.R
+import com.app.srtravels.onboarding.feature.onboarding.OnBoardingActivity
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, OnBoardingActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+}
